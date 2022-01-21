d3a2549c559b248384b93e9e73213cb7eba38f84
DeniziaFernanda <deusine16@gmail.com>
2022-01-21 11:51:50 +0100

    feat: delete user

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const morgan = require("morgan")
+const morgan = require("morgan");
 const { UserModel } = require("./models/userModel");
 const app = express();
 const port = process.env.PORT || 3000;
 
-
-app.use(express.json()) // Vamos utilizar um middleware : Permite que a app uliize json como forma de comunicação.
-app.use(morgan())
-
+app.use(express.json()); // Vamos utilizar um middleware : Permite que a app uliize json como forma de comunicação.
+app.use(morgan()); // Dependencia de Dev, tras uma informacao do que esta a acontecer no servidor. As requests que estao a chegar
 
 mongoose.connect(
   "mongodb://localhost:27017/BD_Loja?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false",
@@ -24,29 +22,54 @@ app.get("/", (req, res) => {
 });
 
 app.get("/users", (req, res) => {
-  UserModel.find().then((value)=>{
+  UserModel.find()
+    .then((value) => {
       res.json({
-        message: value
-      })
-  }).catch((error)=>{
-    res.json({
-      message: "Document not finded"
+        message: value,
+      });
     })
-  })
+    .catch((error) => {
+      res.json({
+        message: error.message,
+      });
+    });
 });
 
+app.delete("/users", (req, res) => {
+  const user = req.body;
+  const id = user._id;
+
+  console.log("--------------------------------------", id);
+  UserModel.deleteOne({ _id: id })
+    .then((value) => {
+      if (value.deletedCount === 0) {
+        res.json({
+          message: "User not founded",
+        }); 
+      } else {
+        res.json({
+          message: "User deleted",
+        });
+      }
+    })
+    .catch((error) => {
+      res.json({
+        message: error.message,
+      });
+    });
+});
 app.post("/users", (req, res) => {
-  const user = req.body
+  const user = req.body;
 
-  UserModel.create(user).then((value)=>{
-    res.json({
-       message: "User registered"
+  UserModel.create(user)
+    .then((value) => {
+      res.json({
+        message: "User registered",
+      });
     })
-  }).catch((error)=>{
-    res.json({
-      message: error.message
-    })
-  })
-})
-
-
+    .catch((error) => {
+      res.json({
+        message: error.message,
+      });
+    });
+});
